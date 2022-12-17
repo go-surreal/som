a79616dceea2ed7fea7c55be232dccc733a51d4e
Marc Binz <code@marcbinz.me>
2022-12-17 17:10:52 +0100

    test: add basic fuzzy testing and benchmark example (#65)

--- example/som_test.go
+++ example/som_test.go
@@ -5,18 +5,17 @@ import (
 	"github.com/marcbinz/som/example/gen/som"
 	"github.com/marcbinz/som/example/gen/som/where"
 	"github.com/marcbinz/som/example/model"
-	"gotest.tools/assert"
-	"testing"
-
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"gotest.tools/assert"
+	"testing"
 )
 
 const (
 	containerName = "som_test_surrealdb"
 )
 
-func TestWithSurreal(t *testing.T) {
+func TestWithDatabase(t *testing.T) {
 	ctx := context.Background()
 
 	req := testcontainers.ContainerRequest{
@@ -56,8 +55,10 @@ func TestWithSurreal(t *testing.T) {
 
 	defer client.Close()
 
+	str := "Some User"
+
 	err = client.User().Create(ctx, &model.User{
-		String: "some_user",
+		String: str,
 	})
 	if err != nil {
 		t.Error(err)
@@ -65,7 +66,7 @@ func TestWithSurreal(t *testing.T) {
 
 	user, err := client.User().Query().
 		Filter(
-			where.User.String.Equal("some_user"),
+			where.User.String.Equal(str),
 		).
 		First()
 
@@ -73,5 +74,141 @@ func TestWithSurreal(t *testing.T) {
 		t.Error(err)
 	}
 
-	assert.Equal(t, "some_user", user.String)
+	assert.Equal(t, str, user.String)
+}
+
+func FuzzWithDatabase(f *testing.F) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "surrealdb/surrealdb:1.0.0-beta.8",
+		Cmd:          []string{"start", "--log", "debug", "--user", "root", "--pass", "root", "memory"},
+		ExposedPorts: []string{"8000/tcp"},
+		WaitingFor:   wait.ForLog("Started web server on 0.0.0.0:8000"),
+	}
+
+	surreal, err := testcontainers.GenericContainer(ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		f.Error(err)
+	}
+
+	defer func() {
+		if err := surreal.Terminate(ctx); err != nil {
+			f.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	endpoint, err := surreal.Endpoint(ctx, "")
+	if err != nil {
+		f.Error(err)
+	}
+
+	client, err := som.NewClient("ws://"+endpoint, "root", "root", "som_test", "som_test")
+	if err != nil {
+		f.Error(err)
+	}
+
+	defer client.Close()
+
+	f.Add("Some User")
+
+	f.Fuzz(func(t *testing.T, str string) {
+		userIn := &model.User{
+			String: str,
+		}
+
+		err = client.User().Create(ctx, userIn)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if userIn.ID == "" {
+			t.Error("user ID must not be empty after create call")
+		}
+
+		userOut, err := client.User().Query().
+			Filter(
+				where.User.ID.Equal(userIn.ID),
+			).
+			First()
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		assert.Equal(t, userIn.String, userOut.String)
+	})
+}
+
+func BenchmarkWithDatabase(b *testing.B) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Image:        "surrealdb/surrealdb:1.0.0-beta.8",
+		Cmd:          []string{"start", "--log", "debug", "--user", "root", "--pass", "root", "memory"},
+		ExposedPorts: []string{"8000/tcp"},
+		WaitingFor:   wait.ForLog("Started web server on 0.0.0.0:8000"),
+	}
+
+	surreal, err := testcontainers.GenericContainer(ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		b.Error(err)
+	}
+
+	defer func() {
+		if err := surreal.Terminate(ctx); err != nil {
+			b.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	endpoint, err := surreal.Endpoint(ctx, "")
+	if err != nil {
+		b.Error(err)
+	}
+
+	client, err := som.NewClient("ws://"+endpoint, "root", "root", "som_test", "som_test")
+	if err != nil {
+		b.Error(err)
+	}
+
+	defer client.Close()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		userIn := &model.User{
+			String: "Some User",
+		}
+
+		err = client.User().Create(ctx, userIn)
+		if err != nil {
+			b.Error(err)
+		}
+
+		if userIn.ID == "" {
+			b.Error("user ID must not be empty after create call")
+		}
+
+		userOut, err := client.User().Query().
+			Filter(
+				where.User.ID.Equal(userIn.ID),
+			).
+			First()
+
+		if err != nil {
+			b.Error(err)
+		}
+
+		assert.Equal(b, userIn.String, userOut.String)
+	}
 }
