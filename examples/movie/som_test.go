9b3b6ccfbfdfc1dd683164f47260443f67a29be3
Marc Binz <code@marcbinz.me>
2023-03-09 23:10:57 +0100

    update

---
+++ examples/movie/som_test.go
@@ -0,0 +1,105 @@
+package basic
+
+import (
+	"context"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	sombase "github.com/marcbinz/som"
+	"github.com/marcbinz/som/examples/movie/gen/som"
+	"github.com/marcbinz/som/examples/movie/gen/som/where"
+	"github.com/marcbinz/som/examples/movie/model"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"gotest.tools/v3/assert"
+	"testing"
+)
+
+const (
+	surrealDBContainerVersion = "nightly" // 1.0.0-beta.8
+	containerName             = "som_test_surrealdb"
+)
+
+func conf(endpoint string) som.Config {
+	return som.Config{
+		Address:   "ws://" + endpoint,
+		Username:  "root",
+		Password:  "root",
+		Namespace: "som_test",
+		Database:  "som_test",
+	}
+}
+
+func TestWithDatabase(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		Name:         containerName,
+		Image:        "surrealdb/surrealdb:" + surrealDBContainerVersion,
+		Cmd:          []string{"start", "--log", "debug", "--user", "root", "--pass", "root", "memory"},
+		ExposedPorts: []string{"8000/tcp"},
+		WaitingFor:   wait.ForLog("Started web server on 0.0.0.0:8000"),
+	}
+
+	surreal, err := testcontainers.GenericContainer(ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+			Reuse:            true,
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		if err := surreal.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	endpoint, err := surreal.Endpoint(ctx, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client, err := som.NewClient(conf(endpoint))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer client.Close()
+
+	if err := client.ApplySchema(); err != nil {
+		t.Fatal(err)
+	}
+
+	title := "Some Movie"
+
+	movieNew := model.Movie{
+		Title: title,
+	}
+
+	movieIn := movieNew
+
+	err = client.Movie().Create(ctx, &movieIn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	movieOut, err := client.Movie().Query().
+		Filter(
+			where.Movie.ID.Equal(movieIn.ID()),
+			where.Movie.Title.Equal(title),
+		).
+		First()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, title, movieOut.Title)
+
+	assert.DeepEqual(t,
+		movieNew, *movieOut,
+		cmpopts.IgnoreUnexported(sombase.Node{}, sombase.Timestamps{}),
+	)
+}
