f8da2b73c5271f710861e7b83cc77c336cfbd7f6
Marc Binz <code@marcbinz.me>
2023-03-20 20:24:22 +0100

    add codegen comment to lib files

--- core/codegen/base.builder.go
+++ core/codegen/base.builder.go
@@ -105,7 +105,7 @@ func (b *build) copyInternalPackage() error {
 
 	for _, file := range files {
 		content := string(file.Content)
-		content = strings.TrimPrefix(content, "//go:build embed\n\n")
+		content = strings.Replace(content, "//go:build embed", codegenComment, 1)
 
 		err := os.WriteFile(filepath.Join(dir, file.Path), []byte(content), os.ModePerm)
 		if err != nil {
