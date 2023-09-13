e4a0a56845a7014ea066350e1cee62d227f5f232
Marc Binz <code@marcbinz.me>
2023-09-13 22:17:44 +0200

    test: rename folder examples to tests (#209)

---
+++ tests/movie/gen.go
@@ -0,0 +1,3 @@
+package basic
+
+//go:generate go run ../../cmd/somgen/main.go ../../tests/movie/model ../../tests/movie/gen/som
---
+++ tests/testing/gen.go
@@ -0,0 +1,3 @@
+package testing
+
+//go:generate go run ../../cmd/somgen/main.go ../../tests/testing/model ../../tests/testing/gen/som
