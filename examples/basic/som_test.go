59042941236211931f7acdaad38307014758bf6a
Marc Binz <code@marcbinz.me>
2023-09-13 08:20:30 +0200

    test: switch to surrealdb beta.12

--- examples/basic/som_test.go
+++ examples/basic/som_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	surrealDBContainerVersion = "1.0.0-beta.11"
+	surrealDBContainerVersion = "1.0.0-beta.12"
 	containerName             = "som_test_surrealdb"
 	containerStartedMsg       = "Started web server on 0.0.0.0:8000"
 )
