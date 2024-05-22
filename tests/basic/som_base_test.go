db2bfd66927ffc8a26c966bee15e751c55c9fa6b
Marc Binz <code@marcbinz.me>
2024-05-22 20:17:04 +0200

    Update tests/basic/som_base_test.go
    Signed-off-by: Marc Binz <code@marcbinz.me>

--- tests/basic/som_base_test.go
+++ tests/basic/som_base_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	surrealDBVersion    = "v1.5.0"
+	surrealDBVersion    = "1.5.0"
 	containerName       = "som_test_surrealdb"
 	containerStartedMsg = "Started web server on "
 )
