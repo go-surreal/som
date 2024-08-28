76d019ad6c4b2ce51bf8bee0959a5ec610d7ee03
Marc Binz <code@marcbinz.me>
2024-08-28 23:18:38 +0200

    update

--- internal/tests/basic/gen.go
+++ internal/tests/basic/gen.go
@@ -1,3 +1,3 @@
 package basic
 
-//go:generate go run ../../cmd/som/main.go -- gen ../../tests/basic/model ../../tests/basic/gen/som
+//go:generate go run ../../../cmd/som/main.go -- gen ../../internal/tests/basic/model ../../internal/tests/basic/gen/som
