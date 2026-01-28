e50d4d47f425168110b11ac0a7d0ea5bdc836520
Marc Binz <code@marcbinz.me>
2026-01-28 13:50:49 +0100

    remove dead code

--- core/codegen/field/context.go
+++ core/codegen/field/context.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Context struct {
-	SourcePkg      string
-	TargetPkg      string
-	Table          Table
-	Element        Element
-	Receiver       *jen.Statement
-	isFromSlice    bool
+	SourcePkg   string
+	TargetPkg   string
+	Table       Table
+	Element     Element
+	Receiver    *jen.Statement
+	isFromSlice bool
 }
 
 func (c Context) pkgLib() string {
@@ -22,10 +22,6 @@ func (c Context) pkgLib() string {
 
 func (c Context) pkgTypes() string {
 	return path.Join(c.TargetPkg, def.PkgTypes)
-}
-
-func (c Context) pkgQuery() string {
-	return path.Join(c.TargetPkg, def.PkgQuery)
 }
 
 func (c Context) pkgDistinct() string {
