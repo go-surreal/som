5ca99adee3ad40a7177ca3534b14d5bd1ce83b6b
Marc Binz <code@marcbinz.me>
2026-03-05 23:25:24 +0100

    cleanup

--- core/embed/som/repo/som.functions.go
+++ core/embed/som/repo/som.functions.go
@@ -2,51 +2,6 @@
 
 package repo
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
-const statusOK = "OK"
-
-type RawQuery[T any] struct {
-	Status string
-	Time   string
-	Result T
-	Detail string
-}
-
-func Unmarshal[M any](respond any) (model M, err error) {
-	var bytes []byte
-
-	if arrResp, isArr := respond.([]any); len(arrResp) > 0 {
-		if dataMap, ok := arrResp[0].(map[string]any); ok && isArr {
-			if _, ok := dataMap["status"]; ok {
-				if bytes, err = json.Marshal(respond); err == nil {
-					var raw []RawQuery[M]
-					if err = json.Unmarshal(bytes, &raw); err == nil {
-						if raw[0].Status != statusOK {
-							err = fmt.Errorf("%s: %s", raw[0].Status, raw[0].Detail)
-						}
-						model = raw[0].Result
-					}
-				}
-				return model, err
-			}
-		}
-	}
-
-	if bytes, err = json.Marshal(respond); err == nil {
-		err = json.Unmarshal(bytes, &model)
-	}
-
-	return model, err
-}
-
-//
-// -- RECORD ID
-//
-
 func newID(table string) string {
 	return table + ":rand()"
 }
