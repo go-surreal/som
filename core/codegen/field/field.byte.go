64bd03aa0e3c753c7f0640e2a79bf443c2cff07a
Marc Binz <code@marcbinz.me>
2026-09-03 22:32:11 +0200

    fix: make schema application idempotent (#535)

--- core/codegen/field/field.byte.go
+++ core/codegen/field/field.byte.go
@@ -37,7 +37,7 @@ func (f *Byte) SchemaStatements(table, prefix string) []string {
 
 	return []string{
 		fmt.Sprintf(
-			"DEFINE FIELD %s ON TABLE %s TYPE %s %s;",
+			"DEFINE FIELD OVERWRITE %s ON TABLE %s TYPE %s %s;",
 			prefix+f.NameDatabase(), table, f.TypeDatabase(), extend,
 		),
 	}
